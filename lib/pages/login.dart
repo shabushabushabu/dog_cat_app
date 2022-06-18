2960b5bd124b648552382b3e5474b0bb17279739
shabushabu <enigma14389@gmail.com>
2022-06-18 16:33:11 +0700

    edit login page styles

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -30,48 +30,78 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: <Widget>[
-            const Image(
-              image: AssetImage("assets/IMG_9185.jpeg"),
-            ),
-            const Text("Dog Cat Matcher Mobile",
-                style: TextStyle(fontSize: 26)),
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  _formEmail = value;
-                });
-              },
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: "Email Address"),
-            ),
-            const SizedBox(height: 10),
-            TextField(
-              onChanged: (value) {
-                setState(() {
-                  _formPassword = value;
-                });
-              },
-              obscureText: true,
-              decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Password"),
-            ),
-            ElevatedButton(
-                onPressed: handleLogin,
-                style: ElevatedButton.styleFrom(
-                  textStyle: const TextStyle(fontSize: 20),
-                  padding: const EdgeInsets.all(16.0)
+        appBar: AppBar(
+          title: const Text("Dog Cat Matcher Mobile")
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              const Image(
+                image: AssetImage("assets/IMG_9185.jpeg"),
+              ),
+
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  "For all animal lovers",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w300,
+                      fontSize: 15),
                 ),
-                child: const Text("Login")),
-            TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/register");
-                },
-                child: const Text("Create New Account"))
-          ],
-        ));
+              ),
+
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 30, right: 30, top: 10, bottom: 10),
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      _formEmail = value;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), labelText: "Email Address"),
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 30, right: 30, top: 10, bottom: 10),
+                child: TextField(
+                  onChanged: (value) {
+                    setState(() {
+                      _formPassword = value;
+                    });
+                  },
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Password"),
+                ),
+              ),
+
+              SizedBox(
+                height:  50,
+                // padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  onPressed: handleLogin,
+                  style: ElevatedButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 16),
+                      padding: const EdgeInsets.all(16.0)
+                  ),
+                  child: const Text ("Login"),
+                ),
+              ),
+
+              TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/register");
+                  },
+                  child: const Text("Create New Account"))
+            ],
+          ),
+        )
+        );
   }
 }
