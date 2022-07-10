bafdbc9db438e7c475b5d258da6f46b6cb3b4a91
shabushabu <enigma14389@gmail.com>
2022-07-10 15:08:48 +0700

    fix image source bug and edit tag display

--- lib/pages/animal_view.dart
+++ lib/pages/animal_view.dart
@@ -3,40 +3,44 @@ import 'package:flutter/material.dart';
 import 'main.dart';
 
 class AnimalViewPage extends StatelessWidget {
-  const AnimalViewPage({
-    Key? key,
-    required this.animal
-  }) : super(key: key);
+  const AnimalViewPage({Key? key, required this.animal}) : super(key: key);
 
   final Animal animal;
 
   @override
   Widget build(BuildContext context) {
+    Widget photo = (animal.photoUrl != '')
+        ? Image.network("http://127.0.0.1:4000${animal.photoUrl}")
+        : const Text("No Photo");
 
-    Widget photo = (animal.photoUrl != "")
-        ? Image(
-      image: AssetImage(animal.photoUrl),
-    )
-        : const Text("No photo");
-
-    List<TextButton> tags = animal.tags.map((tag) =>
-        TextButton(onPressed: null, child: Text(tag))).toList();
+    List<Chip> tags = animal.tags
+        .map((tag) => Chip(
+              labelPadding: const EdgeInsets.only(left: 8.0, right: 8.0),
+              label: Text("#$tag"),
+              onDeleted: null,
+              backgroundColor: Colors.amber[100],
+            ))
+        .toList();
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(animal.name),
-      ),
-      body: Column(
-        children: <Widget>[
-          photo,
-          Text(animal.description),
-          Row(
-            children: tags,
-          ),
-
-        ],
-
-      )
-    );
+        appBar: AppBar(
+          title: Text(animal.name),
+        ),
+        body: Column(
+          children: <Widget>[
+            photo,
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 30, right: 30, top: 10, bottom: 10),
+              child: Text(animal.description,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.w500, fontSize: 16)),
+            ),
+            Wrap(
+              spacing: 15,
+              children: tags,
+            ),
+          ],
+        ));
   }
 }
